2dd2f9fa9134d2a828d3d523b2928f6fb4d886af
thisisfoobar <matt.mcelreath26@gmail.com>
2024-08-05 15:28:37 -0400

    binary tree and tree exercise

--- tree.js
+++ tree.js
@@ -15,20 +15,58 @@ class Tree {
   /** sumValues(): add up all of the values in the tree. */
 
   sumValues() {
-    
+    if (!this.root) return 0;
+    let total = this.root.val;
+
+    function sumValuesHelper(node) {
+      for (let child of node.children) {
+        total += child.val;
+
+        if (child.children.length > 0) sumValuesHelper(child);
+      }
+    }
+
+    sumValuesHelper(this.root);
+    return total;
   }
 
   /** countEvens(): count all of the nodes in the tree with even values. */
 
   countEvens() {
+    if (!this.root) return 0;
 
+    let count = this.root.val % 2 === 0 ? 1 : 0;
+
+    function countEvensHelper(node) {
+      for (let child of node.children) {
+        if (child.val % 2 === 0) count++;
+
+        if (child.children.length > 0) countEvensHelper(child);
+      }
+    }
+
+    countEvensHelper(this.root);
+    return count;
   }
 
   /** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
 
   numGreater(lowerBound) {
+    if (!this.root) return 0;
 
+    let count = this.root.val > lowerBound ? 1 : 0;
+
+    function numGreaterHelper(node) {
+      for (let child of node.children) {
+        if (child.val > lowerBound) count++;
+
+        if (child.children.length > 0) numGreaterHelper(child)
+      }
+    }
+
+    numGreaterHelper(this.root);
+    return count;
   }
 }
 
